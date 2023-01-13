6206a6e415479b724bccf36bf10f3c11fb02b230
AlaricRG <alaric.rougnon.glasson@gmail.com>
2023-01-13 15:01:30 +0100

    Fix : wording "Mode sombre"

--- components/BoutonModeSombre/BoutonModeSombre.test.tsx
+++ components/BoutonModeSombre/BoutonModeSombre.test.tsx
@@ -1,13 +1,15 @@
 import { fireEvent, screen } from '@testing-library/react'
 
-import { renderFakeComponent } from '../../configuration/testHelper'
+import { renderFakeComponent, fakeFrontDependencies } from '../../configuration/testHelper'
 import { BoutonModeSombre } from './BoutonModeSombre'
 
 describe('bouton mode sombre', () => {
+  const { wording } = fakeFrontDependencies
+
   it('toggle le mode sombre au clic sur le bouton mode Sombre', () => {
     // GIVEN
     renderFakeComponent(<BoutonModeSombre />)
-    const boutonModeSombre = screen.getByRole('button', { name: 'Mode Sombre' })
+    const boutonModeSombre = screen.getByRole('button', { name: wording.MODE_SOMBRE })
     const setItem = jest.spyOn(Storage.prototype, 'setItem')
     const removeItem = jest.spyOn(Storage.prototype, 'removeItem')
 
--- configuration/wording/WordingFr.tsx
+++ configuration/wording/WordingFr.tsx
@@ -129,7 +129,7 @@ export class WordingFr implements Wording {
   readonly NAVIGATION_DESKTOP: string = 'Navigation desktop'
   readonly NAVIGATION_MOBILE: string = 'Navigation mobile'
   readonly FERMER: string = 'Fermer'
-  readonly MODE_SOMBRE: string = 'Mode Sombre'
+  readonly MODE_SOMBRE: string = 'Mode sombre'
 
   // Footer
   readonly INFORMATIONS: string = 'Informations'
