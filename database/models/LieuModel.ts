36ecfb7a4bbabee0cf288ec4b29625c826184d56
Fabien Mercier <fabien.mercier@octo.com>
2023-12-02 14:31:11 +0100

    remplacement de public et ! dans LieuModel au niveau des attributs par declare

--- database/models/LieuModel.ts
+++ database/models/LieuModel.ts
@@ -3,73 +3,73 @@ import { Entity, Column, PrimaryColumn } from 'typeorm'
 @Entity({ name: 'lieu' })
 export class LieuModel {
   @PrimaryColumn({ name: 'id', type: 'int' })
-  public id!: number
+  declare id: number
 
   @Column({ length: 255, name: 'nom', type: 'varchar' })
-  public nom!: string
+  declare nom: string
 
   @Column({ length: 255, name: 'adresse', type: 'varchar' })
-  public adresse!: string
+  declare adresse: string
 
   @Column({ length: 5, name: 'code_postal', type: 'varchar' })
-  public codePostal!: string
+  declare codePostal: string
 
   @Column({ length: 255, name: 'ville', type: 'varchar' })
-  public ville!: string
+  declare ville: string
 
   @Column({ length: 255, name: 'site_internet', type: 'varchar' })
-  public siteInternet!: string
+  declare siteInternet: string
 
   @Column({ length: 14, name: 'telephone', type: 'varchar' })
-  public telephone!: string
+  declare telephone: string
 
   @Column({ name: 'prise_de_rendez_vous', type: 'text' })
-  public priseDeRendezVous!: string
+  declare priseDeRendezVous: string
 
   @Column({ name: 'domaine_de_droit', type: 'text' })
-  public domaineDeDroit!: string
+  declare domaineDeDroit: string
 
   @Column({ name: 'latitude', type: 'float' })
-  public latitude!: number
+  declare latitude: number
 
   @Column({ name: 'longitude', type: 'float' })
-  public longitude!: number
+  declare longitude: number
 
   @Column({ length: 255, name: 'e_mail', type: 'varchar' })
-  public eMail!: string
+  declare eMail: string
 
   @Column({ name: 'horaire', type: 'text' })
-  public horaire!: string
+  declare horaire: string
 
   @Column({ length: 255, name: 'departement', type: 'varchar' })
-  public departement!: string
+  declare departement: string
 
   @Column({ length: 255, name: 'region', type: 'varchar' })
-  public region!: string
+  declare region: string
 
   @Column({ name: 'pmr', type: 'boolean' })
-  public pmr!: boolean
+  declare pmr: boolean
 
   @Column({ name: 'pmr_assiste', type: 'boolean' })
-  public pmr_assiste!: boolean
+  declare pmr_assiste: boolean
 
   @Column({ name: 'visuel', type: 'boolean' })
-  public visuel!: boolean
+  declare visuel: boolean
 
   @Column({ name: 'bim', type: 'boolean' })
-  public bim!: boolean
+  declare bim: boolean
 
   @Column({ name: 'lsf', type: 'boolean' })
-  public lsf!: boolean
+  declare lsf: boolean
 
   @Column({ name: 'calme', type: 'boolean' })
-  public calme!: boolean
+  declare calme: boolean
 
   @Column({ name: 'forme', type: 'boolean' })
-  public forme!: boolean
+  declare forme: boolean
 
   @Column({ name: 'commentaire', type: 'text' })
-  public commentaire!: string
+  declare commentaire: string
 
   public distance?: number
 
